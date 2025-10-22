53fc21c61e5a8170ed7af74252227a80821d3c9c
hye <hyejee0221@gmail.com>
2025-10-22 15:05:12 +0900

    검색 컴포넌트

---
+++ todo-practice/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import "./Content.css";
+
+const Content = () => {
+  return (
+    <div className="Content">
+      <div className="search-container">
+        <input placeholder="할 일 검색하기" />
+        <button>🔍</button>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
